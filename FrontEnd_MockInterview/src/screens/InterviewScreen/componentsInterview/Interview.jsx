76912f1c290777cb0f7fe4b8f8fb3f827ed108e5
Kushal Kumar Banyal <kushal1748.be21@chitkara.edu.in>
2025-01-08 18:04:55 +0530

    route fixed

--- FrontEnd_MockInterview/src/screens/InterviewScreen/componentsInterview/Interview.jsx
+++ FrontEnd_MockInterview/src/screens/InterviewScreen/componentsInterview/Interview.jsx
@@ -50,7 +50,7 @@ const Interview = () => {
             <button
               className="px-8 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition duration-300"
               onClick={() => {
-                navigate(`${id}/start`);
+                navigate(`/interview/${id}/start`);
               }}
             >
               Start Interview
